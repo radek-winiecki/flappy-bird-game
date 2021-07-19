4bce43c9951971a2e99886b7526fa3678df712ff
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-07-19 16:44:29 +0200

    Add a conditional statement for a ready and over objects

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const state = {
 }
 
 // control the game
-document.addEventListener("click", function (evt) {
+cvs.addEventListener("click", function (evt) {
    switch (state.current) {
        case state.getReady:
            state.current = state.game;
@@ -81,7 +81,11 @@ const bird = {
         let bird = this.animation[this.frame];
 
         ctx.drawImage(sprite, bird.sX, bird.sY, this.w, this.h, this.x - this.w/2, this.y - this.h/2, this.w, this.h);
-    }
+    },
+
+    flap: function () {
+        
+    },
 }
 
 // get ready
@@ -94,7 +98,9 @@ const getReady = {
     y: 80,
 
     draw: function () {
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        if (this.state.current === state.getReady) {
+            ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        }
     }
 }
 
@@ -108,7 +114,9 @@ const gameOver = {
     y: 90,
 
     draw: function () {
-        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        if (state.current === state.over) {
+            ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+        }
     }
 }
 
