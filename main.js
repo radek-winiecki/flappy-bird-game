9196d0f89f5ad87d7115e94d3bc6e899b4a44adc
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-07-19 13:42:13 +0200

    Create a background object

--- main.js
+++ main.js
@@ -9,6 +9,20 @@ let frames = 0;
 const sprite = new Image();
 sprite.src = "img/sprite.png";
 
+// background
+const bg = {
+    sX: 0,
+    sY: 0,
+    w: 275,
+    h: 226,
+    x: 0,
+    y: cvs.height - 226,
+
+    draw: function () {
+        ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
+    }
+}
+
 // draw
 function draw() {
     ctx.fillStyle = "#70c5ce";
