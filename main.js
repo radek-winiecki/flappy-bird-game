01b651b03707321f096155b9a873f32b2df85dcf
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-07-19 13:34:10 +0200

    Select cvs and create draw, update and loop functions

---
+++ main.js
@@ -0,0 +1,23 @@
+// select cvs
+const cvs = document.getElementById("bird");
+const ctx = cvs.getContext("2d");
+
+// game vars and consts
+let frames = 0;
+
+// draw
+function draw() {
+
+}
+
+// update
+function update() {
+
+}
+
+// loop
+function loop() {
+
+}
+
+loop();
