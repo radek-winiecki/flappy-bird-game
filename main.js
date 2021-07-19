8edc9b894475b46e5a4454ed36480f6275b6e244
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-07-19 22:47:11 +0200

    Set the moving foreground

--- main.js
+++ main.js
@@ -57,9 +57,16 @@ const fg = {
     x: 0,
     y: cvs.height - 112,
 
+    dx: 2,
+
     draw: function () {
         ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x, this.y, this.w, this.h);
         ctx.drawImage(sprite, this.sX, this.sY, this.w, this.h, this.x + this.w, this.y, this.w, this.h);
+    },
+    update: function () {
+        if (state.current === state.game) {
+            this.x = (this.x - this.dx) % (this.w / 2);
+        }
     }
 }
 
@@ -179,6 +186,7 @@ function draw() {
 // update
 function update() {
     bird.update();
+    fg.update();
 }
 
 // loop
