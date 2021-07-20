c0e33d6e10443d1e87d769caa2a1db67fb52a3ef
Radoslaw Winiecki <winiecki.radoslaw@gmail.com>
2021-07-20 11:06:08 +0200

    Load sounds to the project

--- main.js
+++ main.js
@@ -10,6 +10,22 @@ const DEGREE = Math.PI / 180;
 const sprite = new Image();
 sprite.src = "img/sprite.png";
 
+// load sounds
+const SCORE_S = new Audio();
+SCORE_S.src = "audio/sfx_point.wav";
+
+const FLAP = new Audio();
+FLAP.src = "audio/sfx_flap.wav";
+
+const HIT = new Audio();
+HIT.src = "audio/sfx_hit.wav";
+
+const SWOOSHING = new Audio();
+SWOOSHING.src = "audio/sfx_swooshing.wav";
+
+const DIE = new Audio();
+DIE.src = "audio/sfx_die.wav";
+
 // game state
 const state = {
     current: 0,
@@ -31,9 +47,11 @@ cvs.addEventListener("click", function (evt) {
     switch (state.current) {
         case state.getReady:
             state.current = state.game;
+            SWOOSHING.play();
             break;
         case state.game:
             bird.flap();
+            FLAP.play();
             break;
         case state.over:
             let rect = cvs.getBoundingClientRect();
@@ -147,6 +165,7 @@ const bird = {
 
                 if (state.current === state.game) {
                     state.current = state.over;
+                    DIE.play();
                 }
                 // if the speed is greater than the jump - the bird is falling down
                 if (this.speed >= this.jump) {
@@ -246,10 +265,12 @@ const pipes = {
             // top pipe
             if (bird.x + bird.radius > p.x && bird.x - bird.radius < p.x + this.w && bird.y + bird.radius > p.y && bird.y - bird.radius < p.y + this.h) {
                 state.current = state.over;
+                HIT.play();
             }
             // bottom pipe
             if (bird.x + bird.radius > p.x && bird.x - bird.radius < p.x + this.w && bird.y + bird.radius > bottomPipeYPos && bird.y - bird.radius < bottomPipeYPos + this.h) {
                 state.current = state.over;
+                HIT.play();
             }
 
             // move the pipes to the left
@@ -259,6 +280,7 @@ const pipes = {
             if (p.x + this.w <= 0) {
                 this.position.shift();
                 score.value += 1;
+                SCORE_S.play();
 
                 score.best = Math.max(score.value, score.best);
                 localStorage.setItem("best", score.best);
